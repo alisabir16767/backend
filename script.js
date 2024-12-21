0a506dd419b3fb220db22822ad360b008c870e69
Sabir Ali <alisabir167167@gmail.com>
2024-12-21 06:24:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const math = require('./math.js');
+
+// Use the functions from the `math` module
+console.log(math.add(1, 2)); // 3
+console.log(math.subtract(4, 2)); // 2
+console.log(math.multiply(3, 4)); // 12
+console.log(math.divide(10, 5)); // 2
